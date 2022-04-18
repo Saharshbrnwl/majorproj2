5b5218b9b72ba6f80954a8493aa2815d03a3722e
Saharsh Baranwal <saharshbrnwl@gmail.com>
2022-04-19 01:55:56 +0530

    SASS part completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,23 +8,32 @@ module.exports.profile = function(req, res){
 }
 
 
-//render the sign Up page
-module.exports.signUp = function(req,res){
-    return res.render('user_sign_up',{
-        title: "Codeiall | Sign Up"
+// render the sign up page
+module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
+
+
+
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
     })
 }
 
 
-//render the sign In page
-module.exports.signIn = function(req,res){
-    return res.render('user_sign_in',{
-        title: "Codeiall | Sign In"
+// render the sign in page
+module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
     })
 }
 
-//get the sign Up data
-module.exports.create = function(req,res){
+// get the sign up data
+module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
@@ -32,7 +41,7 @@ module.exports.create = function(req,res){
     User.findOne({email: req.body.email}, function(err, user){
         if(err){console.log('error in finding user in signing up'); return}
 
-        if(!user){
+        if (!user){
             User.create(req.body, function(err, user){
                 if(err){console.log('error in creating user while signing up'); return}
 
@@ -41,11 +50,17 @@ module.exports.create = function(req,res){
         }else{
             return res.redirect('back');
         }
+
     });
 }
 
 
-//sign in and create a session for the user 
-module.exports.createSession = function(req,res){
-    //TODO later
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+    req.logout();
+    return res.redirect('/');
 }
