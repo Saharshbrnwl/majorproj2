7860fe7bfe725c8d20c8962661d3a56308c29af9
Saharsh Baranwal <saharshbrnwl@gmail.com>
2022-04-19 20:18:45 +0530

    Displayed posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
+   // console.log(req.cookies);
     
-    return res.render('home', {
-        title: "Home"
-    });
+//without puopulating
+    //Post.find({}, function(err,posts){
+     //   return res.render('home', {
+     //       title: "Codeial | Home",
+    //        posts: posts
+      //  });
+  //  });
+
+    //fetching user details for displaying it below post(populating the user)
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    })
 }
 
 // module.exports.actionName = function(req, res){}
